f57170828a9698bc9d55c900b729af9c67220e83
gao <gxlibra@gmail.com>
2019-03-14 00:19:38 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# __Author__ = 'gaogao'
+
+MONGO_URL = 'localhost'
+MONGO_DB = 'taobao'
+MONGO_COLLECTION = 'products'
+
+KEYWORD = 'ipad'
+
+MAX_PAGE = 100
+
+SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
